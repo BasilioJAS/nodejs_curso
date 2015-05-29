a52e5e3d6c44bb8dc8e3b99fbc9a3dc1c8f3d6e9
zukutrule <sader.ale@gmail.com>
2015-05-29 05:01:37 -0300

    remove temp add server_preguntas.js

--- modulo_4/p2p_1/server_preguntas.js
+++ modulo_4/p2p_1/server_preguntas.js
@@ -1,55 +1,112 @@
+"use strict";
 var express = require ("express");
-//var tag = require("./tag");
-//var path = require ("path");
 var app= express();
 
 app.get("/preguntas", 
 	function (req, res){
-        console.log("asdasdasdasd");
-        var str = createHtmlPreguntas();
-        console.log(str);
-
-        res.send (str);
+        res.send (createHtmlPreguntas());
 	}
 );
 
-app.get("/respuestas", function (req, res){
-	var a = req.query.respuesta;
-	var b = req.query.id;
-	console.log (a);
-	console.log (b);
-
-}
-
+app.get("/respuestas",
+    function (req, res){
+        res.send (createHtmlRespuestas(req.query.respuesta, parseInt(req.query.id)));
+    }
+);
+app.get("*",
+    function (req, res){
+        res.send (createHtmlPreguntas());
+    }
 );
 
-//app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(8000);
 
-function addTabs (count)
+function respuestaCorrecta(id)
 {
-	var str = "";
-	for (var i = 0 ; i<count ; i++)
-	{
-		str += "\t";
-	}
-	return str;
+    switch (id)
+    {
+        case 1:
+            return "los vikingos";
+        break;
+        case 2:
+            return "lisboa";
+        break;
+    }
+    throw "error, id de respuesta erroneo";
 }
-function endLine ()
+function esCorrectaLaRespuesta(respuesta, id)
 {
-	return "\n";
+    if (respuesta === respuestaCorrecta(id))
+    {
+        return true;
+    }
+    else return false;
 }
+function createHtmlRespuestas(respuesta, id)
+{
+    if(esCorrectaLaRespuesta(respuesta, id))
+    {
+        //armo la pagina correcta
+        var html = tag("html");
+        var head = tag("head");
+        var headTitle = tag("title");
+        var headMeta = tag("meta", true);
+        var body = tag("body");
+        var t3 = tag("t3");
 
+        var br = tag("br",true);
+        var aHref = tag("a");
+        aHref.addProperty("href", "./preguntas");
+        aHref.addValue("Back");
+        //<a href="http://www.w3schools.com/html/">Visit our HTML tutorial</a>
+        headTitle.addValue("respuestas");
+        headMeta.addProperty("charset", "UTF-8");
+        t3.addValue("La respuesta '"+respuesta+"' es correcta!");
+        body.addChildren(t3);
 
+        body.addChildren(br);
+        body.addChildren(aHref);
 
-/**/
-
-
+        head.addChildren(headTitle);
+        head.addChildren(headMeta);
+        html.addChildren(head);
+        html.addChildren(body);
+        return  "<!DOCTYPE html>\n" + html.createHtml();
+    }
+    else
+    {
+        respuesta = respuestaCorrecta(id);
+        var html = tag("html");
+        var head = tag("head");
+        var headTitle = tag("title");
+        var headMeta = tag("meta", true);
+        var body = tag("body");
+        var t3 = tag("t3");
+        var br = tag("br",true);
+        var aHref = tag("a");
+        aHref.addProperty("href", "./preguntas");
+        aHref.addValue("Back");
+        headTitle.addValue("respuestas");
+        headMeta.addProperty("charset", "UTF-8");
+        t3.addValue("La respuesta correcta era = '"+respuesta+"'");
+        body.addChildren(t3);
+        body.addChildren(br);
+        body.addChildren(aHref);
+        head.addChildren(headTitle);
+        head.addChildren(headMeta);
+        html.addChildren(head);
+        html.addChildren(body);
+        return  "<!DOCTYPE html>\n" + html.createHtml();
+    }
+}
+/**
+ * ASCOOOOOO
+ * @returns {string}
+ */
 function createHtmlPreguntas()
 {
     var strHtml = "";
-
     var html = tag("html");
     var head = tag("head");
     var headTitle = tag("title");
@@ -66,51 +123,38 @@ function createHtmlPreguntas()
     var bodyFormInputSubmit_2 = tag("input", true);
     var bodyFormT3_2 = tag("t3");
     var br = tag("br",true);
-
     headTitle.addValue("el texto del titulo");
     headMeta.addProperty("charset", "UTF-8");
-
-
     bodyForm_1.addProperty("method", "get");
-    bodyForm_1.addProperty("action", "/preguntas");
+    bodyForm_1.addProperty("action", "/respuestas");
     bodyFormInputText_1.addProperty("type", "text");
     bodyFormInputText_1.addProperty("name", "respuesta");
-    bodyFormInputText_1.addProperty("value", "escriba su respuesta");
+    bodyFormInputText_1.addProperty("value", "");
     bodyFormInputHidden_1.addProperty("type", "hidden");
     bodyFormInputHidden_1.addProperty("name", "id");
-    /**/
     bodyFormInputHidden_1.addProperty("value", 1);
-    bodyFormT3_1.addValue("quien descubrio america")
-    /**/
+    bodyFormT3_1.addValue("quien descubrio america");
     bodyFormInputSubmit_1.addProperty("type", "submit");
     bodyFormInputSubmit_1.addProperty("value", "contestar");
     bodyForm_1.addChildren(bodyFormT3_1);
-
     bodyForm_1.addChildren(bodyFormInputText_1);
     bodyForm_1.addChildren(bodyFormInputHidden_1);
     bodyForm_1.addChildren(bodyFormInputSubmit_1);
-
-
     bodyForm_2.addProperty("method", "get");
-    bodyForm_2.addProperty("action", "/preguntas");
+    bodyForm_2.addProperty("action", "/respuestas");
     bodyFormInputText_2.addProperty("type", "text");
     bodyFormInputText_2.addProperty("name", "respuesta");
-    bodyFormInputText_2.addProperty("value", "escriba su respuesta");
+    bodyFormInputText_2.addProperty("value", "");
     bodyFormInputHidden_2.addProperty("type", "hidden");
     bodyFormInputHidden_2.addProperty("name", "id");
-    /**/
     bodyFormInputHidden_2.addProperty("value", 2);
     bodyFormT3_2.addValue("capital de portugal")
-    /**/
     bodyFormInputSubmit_2.addProperty("type", "submit");
     bodyFormInputSubmit_2.addProperty("value", "contestar");
     bodyForm_2.addChildren(bodyFormT3_2);
-
     bodyForm_2.addChildren(bodyFormInputText_2);
     bodyForm_2.addChildren(bodyFormInputHidden_2);
     bodyForm_2.addChildren(bodyFormInputSubmit_2);
-
-
     body.addChildren(bodyForm_1);
     body.addChildren(br);
     body.addChildren(bodyForm_2);
@@ -119,11 +163,16 @@ function createHtmlPreguntas()
     html.addChildren(head);
     html.addChildren(body);
     strHtml = "<!DOCTYPE html>\n" + html.createHtml();
-    //console.log(strHtml);
     return strHtml;
 }
-//"use strict";
 
+/**
+ * esta clase se encarga de crear un tag HTML
+ * @param tagName
+ * @param close
+ * @returns {{createHtml: createHtml, addValue: addValue, addProperty: addProperty, addChildren: addChildren}}
+ */
+//TODO: averiguar como meter herencia y poder crear clases menos genericas que hereden de esta
 function tag (tagName, close)
 {
     var _close = close || false;
@@ -132,6 +181,11 @@ function tag (tagName, close)
     var _prop = {};
     var _value = "";
 
+    /**
+     * Recorre todos los hijos y los va agregando al string que va a devolver, agregado tabs y enters donde corresponda
+     * @param tabPos
+     * @returns {string}
+     */
     function createHtml(tabPos) {
         var tabPosition = tabPos || 0;
         var htmlText = "";
@@ -162,18 +216,36 @@ function tag (tagName, close)
         return htmlText;
     }
 
+    /**
+     * hay que agregar todos los hijos ordenadamente
+     * @param children
+     */
     function addChildren(children) {
         _childrens.push(children);
     }
 
+    /**
+     * Sirve para crear tags tipo <T1> o <A>
+     * @param value
+     */
     function addValue(value) {
         _value = value;
     }
 
+    /**
+     * se tiene que ingresar la dupla nombre valor. Ej. para agregar href="google.com" a un <A>
+     * @param name
+     * @param value
+     */
     function addProperty(name, value) {
         _prop[name] = value;
     }
 
+    /**
+     * para que el codigo quede lindo
+     * @param count
+     * @returns {string}
+     */
     function addTabs(count) {
         var str = "";
         for (var i = 0; i < count; i++) {
@@ -182,6 +254,10 @@ function tag (tagName, close)
         return str;
     }
 
+    /**
+     * idem anterior
+     * @returns {string}
+     */
     function endLine() {
         return "\n";
     }
@@ -193,20 +269,3 @@ function tag (tagName, close)
         addChildren: addChildren
     }
 }
-
-
-
-/**/
-
-
-
-
-
-
-
-
-
-
-
-
-
